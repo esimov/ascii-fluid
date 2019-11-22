64b94dee15f0486f63643fdb8af6f0513e12e815
esimov <esimovaq@yahoo.com>
2019-11-22 12:49:27 +0200

    Concurrent terminal render

--- fluid-solver/particle.go
+++ fluid-solver/particle.go
@@ -1,28 +1,60 @@
 package fluid
 
 type Particle struct {
-	x, y   int
+	x, y   float64
 	vx, vy float64
-	age    int
+	age    float64
 	dead   bool
 }
 
-func NewParticle(x, y int) *Particle {
+func NewParticle(x, y float64) *Particle {
 	return &Particle{x: x, y: y}
 }
 
-func (p *Particle) GetX() float64 {
-	return p.vx
+func (p *Particle) GetX() int {
+	return int(p.x)
 }
 
 func (p *Particle) SetX(val float64) {
-	p.vx = val
+	p.x = val
 }
 
-func (p *Particle) GetY() float64 {
-	return p.vy
+func (p *Particle) GetY() int {
+	return int(p.y)
 }
 
 func (p *Particle) SetY(val float64) {
+	p.y = val
+}
+
+func (p *Particle) GetVx() int {
+	return int(p.vx)
+}
+
+func (p *Particle) SetVx(val float64) {
+	p.vx = val
+}
+
+func (p *Particle) GetVy() int {
+	return int(p.vy)
+}
+
+func (p *Particle) SetVy(val float64) {
 	p.vy = val
+}
+
+func (p *Particle) GetAge() float64 {
+	return p.age
+}
+
+func (p *Particle) SetAge(age float64) {
+	p.age = age
+}
+
+func (p *Particle) GetDeath() bool {
+	return p.dead
+}
+
+func (p *Particle) SetDeath(dead bool) {
+	p.dead = dead
 }
--- terminal/terminal.go
+++ terminal/terminal.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"sync"
 	"time"
 	"unicode/utf8"
 
@@ -31,15 +32,23 @@ type options struct {
 	grayscale         bool
 }
 
-const numOfCells = 128 // Number of cells (not including the boundary)
+const (
+	numOfCells         = 128 // Number of cells (not including the boundary)
+	particleTimeToLive = 5
+)
 
 var (
-	rnd         *rand.Rand
-	startTime   time.Time
+	rnd       *rand.Rand
+	startTime time.Time
+
 	isMouseDown bool
 	oldMouseX   int
 	oldMouseY   int
-	particles   []*fluid.Particle
+
+	termWidth  int
+	termHeight int
+	cellSize   int
+	particles  []*fluid.Particle
 )
 
 func init() {
@@ -55,12 +64,6 @@ func New() *Terminal {
 }
 
 func (t *Terminal) Init() *Terminal {
-	startTime = time.Now()
-	isMouseDown = false
-	oldMouseX = 0
-	oldMouseY = 0
-	particles = make([]*fluid.Particle, 0)
-
 	t.opts = &options{
 		drawVelocityField: false,
 		drawDensityField:  true,
@@ -76,6 +79,15 @@ func (t *Terminal) Init() *Terminal {
 	t.fs = fluid.NewSolver(termbox.Size())
 	t.fs.ResetVelocity()
 
+	startTime = time.Now()
+	isMouseDown = false
+	oldMouseX = 0
+	oldMouseY = 0
+	particles = make([]*fluid.Particle, 0)
+
+	termWidth, termHeight = termbox.Size()
+	cellSize = termWidth / numOfCells
+
 	return t
 }
 
@@ -85,29 +97,49 @@ func (t *Terminal) Render() {
 
 	termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
 	t.reallocBackBuffer(termbox.Size())
-	t.redraw(-1, -1)
+
+	wg := sync.WaitGroup{}
+	ticker := time.Tick(time.Millisecond * time.Duration(5))
+
+	eventQueue := make(chan termbox.Event)
+	go func() {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
 
 mainloop:
 	for {
 		mx, my := -1, -1
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			if ev.Key == termbox.KeyEsc {
-				break mainloop
+		select {
+		case ev := <-eventQueue:
+			switch ev.Type {
+			case termbox.EventKey:
+				if ev.Key == termbox.KeyEsc {
+					break mainloop
+				}
+			case termbox.EventMouse:
+				if ev.Key == termbox.MouseLeft {
+					isMouseDown = true
+					mx, my = ev.MouseX, ev.MouseY
+					t.onMouseMove(mx, my)
+					//t.log(t.logfile, "X:%d \t Y:%d\n", mx, my)
+
+					//t.redraw(mx, my)
+				}
+			case termbox.EventResize:
+				t.reallocBackBuffer(ev.Width, ev.Height)
 			}
-		case termbox.EventMouse:
-			if ev.Key == termbox.MouseLeft {
-				mx, my = ev.MouseX, ev.MouseY
-				t.onMouseMove(ev)
-				t.log(t.logfile, "X:%d \t Y:%d\n", mx, my)
-			}
-			if ev.Key == termbox.MouseRight {
-				isMouseDown = true
-			}
-		case termbox.EventResize:
-			t.reallocBackBuffer(ev.Width, ev.Height)
+		default:
+			wg.Add(1)
+			<-ticker
+			go func() {
+				defer wg.Done()
+				t.update()
+			}()
+			wg.Wait()
+			time.Sleep(10 * time.Millisecond)
 		}
-		t.redraw(mx, my)
 	}
 }
 
@@ -131,17 +163,10 @@ func (t *Terminal) redraw(mx, my int) {
 	termbox.Flush()
 }
 
-func (t *Terminal) log(f io.Writer, format string, vals ...interface{}) {
-	fmt.Fprintf(f, format, vals...)
-}
-
-func (t *Terminal) onMouseMove(event termbox.Event) {
-	mouseX, mouseY := event.MouseX, event.MouseY
-	width, height := termbox.Size()
-
+func (t *Terminal) onMouseMove(mouseX, mouseY int) {
 	// Find the cell below the mouse
-	i := int(math.Abs(float64(mouseX)/float64(width))*numOfCells) + 1
-	j := int(math.Abs(float64(mouseY)/float64(height))*numOfCells) + 1
+	i := int(math.Abs(float64(mouseX)/float64(termWidth))*numOfCells) + 1
+	j := int(math.Abs(float64(mouseY)/float64(termHeight))*numOfCells) + 1
 
 	// Dont overflow grid bounds
 	if i > numOfCells || i < 1 || j > numOfCells || j < 1 {
@@ -151,10 +176,11 @@ func (t *Terminal) onMouseMove(event termbox.Event) {
 	// Mouse velocity
 	du := float64(mouseX-oldMouseX) * 1.5
 	dv := float64(mouseY-oldMouseY) * 1.5
+	fmt.Println(du, dv)
 
 	// Add the mouse velocity to cells above, below, to the left, and to the right as well.
 	t.fs.SetCell("uOld", i, j, du)
-	t.log(t.logfile, "Cell: %v\n", t.fs.GetCell("uOld", i, j))
+	//t.log(t.logfile, "Cell: %v\n", t.fs.GetCell("uOld", i, j))
 	t.fs.SetCell("vOld", i, j, dv)
 
 	t.fs.SetCell("uOld", i+1, j, du)
@@ -171,14 +197,17 @@ func (t *Terminal) onMouseMove(event termbox.Event) {
 
 	if isMouseDown {
 		// If holding down the right mouse, add density to the cell below the mouse
-		t.fs.SetCell("dOld", i, j, 50)
+		t.fs.SetCell("dOld", i, j, 10)
 	}
 
 	if isMouseDown && t.opts.drawParticles {
-		for k := 0; k < 5; k++ {
-			p := fluid.NewParticle(mouseX+random(rnd, -50, 50), mouseY+random(rnd, -50, 50))
-			p.SetX(du)
-			p.SetY(dv)
+		for i := 0; i < 5; i++ {
+			p := fluid.NewParticle(
+				float64(mouseX)+random(rnd, -10, 10),
+				float64(mouseY)+random(rnd, -10, 10),
+			)
+			p.SetVy(du)
+			p.SetVy(dv)
 
 			particles = append(particles, p)
 		}
@@ -188,6 +217,108 @@ func (t *Terminal) onMouseMove(event termbox.Event) {
 	oldMouseY = mouseY
 }
 
-func random(rnd *rand.Rand, min, max int) int {
-	return rnd.Intn(max-min) + min
+func (t *Terminal) update() {
+	endTime := time.Now()
+	dt := endTime.Sub(startTime).Seconds()
+
+	t.fs.VelocityStep()
+	t.fs.DensityStep()
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	if t.opts.drawVelocityField {
+		// TODO implement me
+	}
+
+	for i := 0; i < len(particles); i++ {
+		p := particles[i]
+		p.SetAge(float64(p.GetAge()) + dt)
+
+		alpha := float64(1 - p.GetAge()/particleTimeToLive)
+		//fmt.Println(alpha)
+		if alpha < 0.001 ||
+			p.GetAge() >= particleTimeToLive ||
+			p.GetX() <= 0 || p.GetX() >= termWidth ||
+			p.GetY() <= 0 || p.GetY() >= termHeight {
+			p.SetDeath(true)
+		} else {
+			x0 := int(math.Abs(float64(p.GetX())/float64(termWidth))*numOfCells) + 2
+			y0 := int(math.Abs(float64(p.GetY())/float64(termHeight))*numOfCells) + 2
+
+			//t.log(t.logfile, "X: %v \t Y:%v\n", x0, y0)
+			//fmt.Println(float64(p.GetX())/float64(termWidth), p.GetY()/termHeight)
+			//fmt.Println(t.fs.GetCell("u", x0, y0))
+
+			p.SetVx(t.fs.GetCell("u", x0, y0) * 50)
+			p.SetVy(t.fs.GetCell("v", x0, y0) * 50)
+
+			p.SetX(float64(p.GetX() + p.GetVx()))
+			p.SetY(float64(p.GetY() + p.GetVy()))
+
+			//fmt.Println(p.GetX(), p.GetY())
+
+			attrf := func() (rune, termbox.Attribute, termbox.Attribute) {
+				return '·', termbox.ColorDefault, termbox.ColorDefault
+			}
+			r, fg, bg := attrf()
+			//termbox.SetCell(int(random(rnd, 0, termWidth)), int(random(rnd, 0, termHeight)), r, fg, bg)
+			// rn := random(rnd, -3, 3)
+			// termbox.SetCell(p.GetX()+int(rn), p.GetY()+int(rn), r, fg, bg)
+			termbox.SetCell(p.GetX(), p.GetY(), r, fg, bg)
+			//t.log(t.logfile, "X: %v \t Y:%v\n", p.GetX(), p.GetY())
+		}
+
+		if p.GetDeath() {
+			// Remove dead particles, and update the length manually
+			particles = append(particles[:i], particles[i+1:]...)
+		}
+	}
+	startTime = time.Now()
+
+	// // Render fluid
+	// for i := 1; i <= numOfCells; i++ {
+	// 	// the x position of the current cell
+	// 	//dx := (float64(i) - 0.5) * float64(cellSize)
+
+	// 	for j := 1; j <= numOfCells; j++ {
+	// 		// the y position of the current cell
+	// 		//dy := (float64(j) - 0.5) * float64(cellSize)
+
+	// 		//density := t.fs.GetCell("d", i, j)
+	// 		if t.opts.drawDensityField {
+	// 			for l := 0; l < cellSize; l++ {
+	// 				for m := 0; m < cellSize; m++ {
+	// 					mx := (i-1)*cellSize + l
+	// 					my := (j-1)*cellSize + m
+	// 					//pxIdx := pxX + pxY*terminalSize*4
+
+	// 					t.log(t.logfile, "x:%v\t y:%v\n", mx, my)
+
+	// 					attrf := func() (rune, termbox.Attribute, termbox.Attribute) {
+	// 						return '█', termbox.ColorDefault, termbox.ColorWhite
+	// 					}
+	// 					r, fg, bg := attrf()
+	// 					termbox.SetCell(mx, my, r, fg, bg)
+	// 				}
+	// 			}
+	// 		}
+
+	// 	}
+	// }
+	termbox.Flush()
+}
+
+// func (t *Terminal) getRune() rune {
+// 	for i := 1; i <= termWidth; i++ {
+// 		range := i * termHeight + 1
+// 		retval :=
+// 	}
+// }
+
+func (t *Terminal) log(f io.Writer, format string, vals ...interface{}) {
+	fmt.Fprintf(f, format, vals...)
+}
+
+func random(rnd *rand.Rand, min, max int) float64 {
+	return float64(rnd.Intn(max-min) + min)
 }
