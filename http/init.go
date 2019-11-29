b4e4d4f5913f265e56c4917147fc2227c4e9db24
esimov <esimovaq@yahoo.com>
2019-11-29 15:16:48 +0200

    Separate terminal app from webapp

---
+++ http/init.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/esimov/ascii-fluid/wasm/websocket"
+)
+
+func main() {
+	websocket.Init()
+}
--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"github.com/esimov/ascii-fluid/terminal"
-	"github.com/esimov/ascii-fluid/wasm/websocket"
 )
 
 func main() {
 	term := terminal.New()
 	term.Init().Render()
-
-	websocket.Init()
 }
